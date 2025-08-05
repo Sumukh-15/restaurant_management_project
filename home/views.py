bb52ebb7a57b2ece9d2f40408ecb6eafa1b62c09
Sumukh😎 <sumukhmallikarjuna15@gmail.com>
2025-08-05 13:20:32 +0530

    20302

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-from products
+from products.models import Menu
+
 # Create your views here.
+def homepage(request):
+    menu_items = Menu.objects.all()
+    return render(request,'home/homepage.html',{'menu_items':menu_items})

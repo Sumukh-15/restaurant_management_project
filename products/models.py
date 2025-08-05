45b4851056ec33f6fc630b321d68692c1e483ad8
Sumukh😎 <sumukhmallikarjuna15@gmail.com>
2025-08-05 13:12:27 +0530

    20302

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
+    item_description = models.TextField()
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 

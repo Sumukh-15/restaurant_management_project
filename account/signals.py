e9f49b04a1cad83e3d91c718461d2dcb0605df5a
Sumukh😎 <sumukhmallikarjuna15@gmail.com>
2025-08-05 12:57:47 +0530

    20301

--- account/signals.py
+++ account/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth.model import User
 from .models import UserProfile
 
 @receiver(post_save,sender=User)
@@ -9,5 +9,7 @@ def create_user_profile(sender,instance,created,**kwargs):
         UserProfile.objects.create(user=instance)
 
 @reciever(post_save,sender=User)
-def save_user_profie(sender, instance,**kwargs):
-    instance.profile.save()
+def save_create_user(sender, instance, **kwargs):
+    instnace.profile.save()
+
+

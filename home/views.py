13b0df060745d9e841c6a83fafb9dd24f26372e0
Sumukh😎 <sumukhmallikarjuna15@gmail.com>
2025-08-05 15:49:19 +0530

    22202

--- home/views.py
+++ home/views.py
@@ -4,4 +4,8 @@ from products.models import Menu
 # Create your views here.
 def homepage(request):
     menu_items = Menu.objects.all()
+    context={
+        'menu_items':menu_items,
+        'restaurant_name':settings.RESTAURANT_NAME
+    }
     return render(request,'home/homepage.html',{'menu_items':menu_items})

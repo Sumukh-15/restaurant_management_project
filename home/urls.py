4b871ecfcd585c2531e0212a997fa97cda523728
Sumukh😎 <sumukhmallikarjuna15@gmail.com>
2025-08-12 12:32:15 +0530

    22259

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('',views.homepage, name='homepage'),
     path('about/',views.about,name='about'),
+    path('contact/',views.contact_us,name='contact_us'),
 ]

f85e55d99002944b508d089d0d9bc2e461fa7312
Sumukh😎 <sumukhmallikarjuna15@gmail.com>
2025-08-13 17:31:38 +0530

    31228

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.homepage, name='homepage'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact_us,name='contact_us'),
+    path('reservations/',views.reservations,name='reservations'),
 ]

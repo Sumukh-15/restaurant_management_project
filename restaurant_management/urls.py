e1328c22bfd86db5e7459cc947810861a84d7e6c
Sumukh😎 <sumukhmallikarjuna15@gmail.com>
2025-09-07 22:16:18 +0530

    39457

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
-    path
+    path('',include('home.ur;s')),
 ]

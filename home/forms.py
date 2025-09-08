448c1c9d4f4db13731f894305ec9733b64bb2f82
Sumukh😎 <sumukhmallikarjuna15@gmail.com>
2025-09-08 09:33:59 +0530

    39455

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from .models import Feedback
 
 class ContactForm(forms.Form):
     name=forms.CharField(max_length=100)
     email=forms.EmailField()
+
+class FeedBackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['comment']
+        widgets = {
+            'comment':forms.Textarea(attrs = {'rows':4,'placeholder':'Leave your feedback here...'})
+        }

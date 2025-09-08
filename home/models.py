193deeb265cf194dcf75284b3e5301099c846e80
Sumukh😎 <sumukhmallikarjuna15@gmail.com>
2025-09-08 09:30:08 +0530

    39455

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    comment = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add =True)
+
+    def __str__(self):
+        return f"Feedback #{self.id} at {self.submitted_at}"
